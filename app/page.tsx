d1c00fb6381f3a2472ca566bd7a7715fc916832c
Arman <fakidarman@gmail.com>
2024-07-15 19:32:52 +0600

    header added

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
    <div>This is will be home page</div>
    //another text
+   <h1>Here will be header</h1>
+    </>
   );
 }
