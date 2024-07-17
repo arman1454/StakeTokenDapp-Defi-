b341f1909b7124158fda9a79ed3939df57949347
Arman <fakidarman@gmail.com>
2024-07-17 21:41:07 +0600

    added typesafety to some of the function

--- utils/handleAccountChange.ts
+++ utils/handleAccountChange.ts
@@ -1,9 +1,27 @@
-export const handleAccountChange = async (setState,updateWallet) => {
-    const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts"
-    })
-    const selectedAccount = accounts[0];
-    console.log(selectedAccount)
-    setState(prevState => ({ ...prevState, selectedAccount }))
-    updateWallet({selectedAccount})
+import { Dispatch, SetStateAction } from 'react';
+
+interface WalletState {
+    provider: any | null;
+    selectedAccount: string | null;
+    stakingContract: any | null;
+    stakeTokenContract: any | null;
+    chainId: number | null;
 }
+export const handleAccountChange = async (setState: Dispatch<SetStateAction<WalletState>>, updateWallet: (newWallet: Partial<WalletState>) => void
+): Promise<void> => {
+    try {
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts"
+        });
+        const selectedAccount = accounts[0];
+        console.log(selectedAccount);
+
+        // Update local state
+        setState(prevState => ({ ...prevState, selectedAccount }));
+
+        // Update global state
+        updateWallet({ selectedAccount });
+    } catch (error) {
+        console.error("Error changing account: ", error);
+    }
+};
--- utils/handleChainChange.ts
+++ utils/handleChainChange.ts
@@ -1,11 +1,28 @@
-export const handleChainChange = async (setState,updateWallet) => {
-    let chainIdHex = await window.ethereum.request(
-        { method: 'eth_chainId' }
-    )
-    const chainId = parseInt(chainIdHex, 16);
-    console.log(chainId)
-    setState(prevState => ({ ...prevState, chainId }))
-    updateWallet({
-        chainId
-    })
+import { Dispatch, SetStateAction } from 'react';
+
+interface WalletState {
+    provider: any | null;
+    selectedAccount: string | null;
+    stakingContract: any | null;
+    stakeTokenContract: any | null;
+    chainId: number | null;
 }
+
+export const handleChainChange = async (
+    setState: Dispatch<SetStateAction<WalletState>>,
+    updateWallet: (newWallet: Partial<WalletState>) => void
+): Promise<void> => {
+    try {
+        const chainIdHex = await window.ethereum.request({ method: 'eth_chainId' });
+        const chainId = parseInt(chainIdHex, 16);
+        console.log(chainId);
+
+        // Update local state
+        setState(prevState => ({ ...prevState, chainId }));
+
+        // Update global state
+        updateWallet({ chainId });
+    } catch (error) {
+        console.error("Error changing chain: ", error);
+    }
+};
